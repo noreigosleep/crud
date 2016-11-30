ac2e9db08c48aadc9a2bc24308d627a8c481e215
Khanh Le <khanhleviet@hotmail.com>
2016-11-30 15:40:36 +0700

    Fixed default page

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -1,5 +1,15 @@
 <?php
     class Product extends AppModel {
+        public $validate = array(
+            'name' => array(
+                'require' => true,
+                'rule' => 'notBlank'
+            ),
+            'category_id' => array(
+                'require' => true,
+                'rule' => 'notBlank'
+            )
+        );
         public $belongsTo = 'Category';
         public function isOwnedBy( $product, $user) {
             return $this->find('count', array('conditions' => array('Category.user_id' => $user, 'Product.id' => $product))) ? true : false;
